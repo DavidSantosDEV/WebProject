f43edfd35db6fc19d01f1cdde0738cca651bfca2
David Machado Monteiro <2211849@my.ipleiria.pt>
2021-10-28 13:54:37 +0100

    Some new implementation
    - Responsive Menu
    - Burguermenu implementation
    - Menu Phone show script in progress

---
+++ Main/js/script.js
@@ -0,0 +1,10 @@
+function showPhoneMenu() {
+  var x = document.getElementById("phone_menu");
+  if (x.Style.visibility === "hidden") {
+    x.Style.display = "flex";
+	x.Style.visibility = "visible";
+  } else {
+    x.Style.display = "none";
+	x.Style.visibility = "hidden";
+  }
+}
